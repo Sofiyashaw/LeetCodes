c7900815cdc64d4f178c86e4992ba44cbdfc1c28
Acinta Sofiya <145840402+Sofiyashaw@users.noreply.github.com>
2024-11-12 12:16:20 +0530

    Update Majority Element.py

--- Majority Element.py
+++ Majority Element.py
@@ -1,30 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 21 00:30:17 2024
-
-@author: jacin
-"""
-
 class Solution(object):
     def majorityElement(self, nums):
-       candidate=None
-       count=0
-
-       for num in nums:
-         if count==0:
-            candidate=num
-            count=1
-         elif num==candidate:
-            count+=1
-         else:
-            count-=1
-
-       count=0
-       for num in nums:
-            if num==candidate:
-                count+=1
-       return candidate
-
-sol=Solution()
-nums = [3,2,3]                           
-sol.majorityElement(nums)        
+        hashmap = {}
+        i = 0
+        while i < len(nums):
+            if nums[i] not in hashmap:
+                hashmap[nums[i]] = 1
+            else:
+                hashmap[nums[i]] += 1
+            i += 1
+        result = max(hashmap, key=hashmap.get)
+        return result
+        
